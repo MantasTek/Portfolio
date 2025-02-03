74b060b6baf481256ea692c105219d88c63543bc
mantas petrovas <mantas10@gmail.com>
2025-02-03 12:48:22 +0100

    Fixed naming of files

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
     return (
         <section className="hero">
             <div className="hero-content">
-                <img src="/images/my photo.jpg" alt="Mantas" className="portrait" />
+                <img src="/images/myphoto.jpg" alt="Mantas" className="portrait" />
                 <h1>Mantas Petrovas</h1>
                 <p className="tagline">Program Developer .NET</p>
                 <div className="hero-buttons">
